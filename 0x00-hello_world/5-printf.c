7156d12917db823a9386813ed5742c033b0f3dec
kelemul <kelemu3509@gmail.com>
2022-06-08 04:40:38 -0700

    another commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ *FileName: 5-printf.c
+ * Author:kelemu leykun
+ */
+
+#include <stdio.h>
+
+/*
+ * main - Prints "this will print the following string 
+ *                " followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+}
