d7b5176e97f6b1d5b981fc00b5ffee720cb44576
kelemul <kelemu3509@gmail.com>
2022-06-08 08:25:40 -0700

    gg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
+/*
+ * file:putchar
+ * Author:kelemu
+ */
+
 #include <stdio.h>
+
 int main(void)
 {
-		puts("\"_putchar");
-			return (0);
+	printf( "_Putchar");
+	printf("\n");
+	
+	return (0);
 }
