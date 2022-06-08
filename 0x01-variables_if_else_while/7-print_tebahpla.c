bec49bb5635af6dfa55d62c4f3bcaac4e92321e9
kelemul <kelemu3509@gmail.com>
2022-06-08 07:52:24 -0700

    another alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/*
+ * File NAme:print alpha 7
+ * Author: Kelemu Leykun
+ *
+ */
+#include <stdio.h>
+
+/**
+ *
+ * main - prints lower case english apha bets
+ *
+ * Return: is 0.
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
