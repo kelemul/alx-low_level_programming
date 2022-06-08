f07bfe2835330380d8aca4e1def1cbde6dbb69f5
kelemul <kelemu3509@gmail.com>
2022-06-08 05:41:02 -0700

    holla c dump

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,14 @@
 #include <unistd.h>
 
 /**
- * the main function will print "the string in the main function",
+ * the main function will print "the string in the main 
+ * function"
  * followed by a new line.
  * the Return: Always 1.
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	
+		return (1);
 }
