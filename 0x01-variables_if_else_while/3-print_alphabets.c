16acb40c56fab2c12e83acb1396e44876dc4f826
kelemul <kelemu3509@gmail.com>
2022-06-08 07:30:09 -0700

    done alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+ * File :print alphabets
+ * Author: Kelemu leykun
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints english alphabets
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter ='a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter ='A'; letter <='Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
