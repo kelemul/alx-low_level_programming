9e21f507b4840dd781729e13090e057e229dac18
kelemul <kelemu3509@gmail.com>
2022-06-08 05:38:56 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,12 @@
 #include <unistd.h>
 
 /**
- * the main function will print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
+ * the main function will print "the string in the main function",
+ * followed by a new line.
  * the Return: Always 1.
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
