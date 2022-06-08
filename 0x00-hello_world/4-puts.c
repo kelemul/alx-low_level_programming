3d0809552b16bda352ae4ed849589cad6d5a65c2
kelemul <kelemu3509@gmail.com>
2022-06-08 04:19:34 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,9 @@
  *Author:Kelemu leykun
  *email: kelemu3509@gmail.com
  */
+
 #include <stdio.h>
+
 /**
  *this program will print the following string in the function body
  *
@@ -12,10 +14,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	/* 
-	 * this will return 0
-	 */
-
 	return (0);
 }
