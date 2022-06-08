1ba33e1eb14896fa60405c6c4d702aa4d3ea5553
kelemul <kelemu3509@gmail.com>
2022-06-08 07:40:39 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * file: Numebrs
+ * Author: kelemu leykun
+ *
+ */
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: always zero 0.
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printff('\n');
+
+	return (0);
+}
