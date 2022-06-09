180dcb26154972e7502988a3fa0be47d279bc406
kelemul <kelemu3509@gmail.com>
2022-06-09 08:27:05 -0700

    done mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: mul
+ * Author: kelemu l
+ */
+
+#include "main.h"
+
+/**
+ * mul - do multiplication
+ * @a: first num
+ * @b: second num
+ *
+ * Return: will provide the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
