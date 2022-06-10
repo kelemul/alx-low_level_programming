dba3a5a468a03223de40ccf82e6273a271a832f2
kelemul <kelemu3509@gmail.com>
2022-06-10 01:54:46 -0700

    fizzfuzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+/*
+ * File: fiz buzz game
+ * Author: kelemu
+ */
+
+#include <studio.h>
+
+/**
+ * main -prints a number anfd Fiz for multiples of 3
+ * buzz for multiples of 5 and FizzBuzz for
+ * multiples of 3 and 5
+ *
+ * Return: is 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+		
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
