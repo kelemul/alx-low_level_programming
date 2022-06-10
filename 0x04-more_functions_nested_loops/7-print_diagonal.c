908078eb43fcd972013edf7602d9b0fef8d26aa5
kelemul <kelemu3509@gmail.com>
2022-06-10 01:41:12 -0700

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+/*
+ * File: printdiagonal.c
+ * Author:kelemu L.
+ */
+
+#include "main.h"
+
+/**
+ * print_diagonal - will draw a diagonal line
+ * @n: the number of chars to create diagonal line
+ */
+void print_diagonal(int n)
+{
+	int len, space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
