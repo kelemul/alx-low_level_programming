85b12f66354c9615396828a731b1425913b6add1
kelemul <kelemu3509@gmail.com>
2022-06-13 03:23:51 -0700

    first result

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: KELEMU L. 
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 -take a pointer and convert to 98.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
