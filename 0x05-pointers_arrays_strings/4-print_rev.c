8da8362471cffd947a8294ec501add8b3440dcc2
kelemul <kelemu3509@gmail.com>
2022-06-13 04:14:41 -0700

    don

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,29 +1,23 @@
 /*
- * File: 5-rev_string.c
+ * File: 4-print_rev.c
  * Auth: KELEMU L.
  */
 
 #include "main.h"
 
 /**
- * rev_string - Reverses a string.
- * @s: The string to be reversed.
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void print_rev(char *s)
 {
-	int len = 0, index = 0;
-	char tmp;
+	int len = 0, index=0;
 
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= len / 2; index--)
-	{
+	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
-	}
 
 	_putchar('\n');
 }
