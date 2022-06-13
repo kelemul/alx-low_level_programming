e943c3908d911a319a7cc9062b97d30eaff42f4b
kelemul <kelemu3509@gmail.com>
2022-06-13 04:04:49 -0700

    done reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,29 @@
 /*
- * File: 4-print_rev.c
+ * File: 5-rev_string.c
  * Auth: KELEMU L.
  */
 
 #include "main.h"
 
 /**
- * print_rev - Prints a string in reverse.
+ * rev_string - Reverses a string.
  * @s: The string to be reversed.
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0, index = 0;
+	char tmp;
 
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index = len - 1; index >= len / 2; index--)
+	{
 		_putchar(s[index]);
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
 
 	_putchar('\n');
 }
