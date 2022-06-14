afef0895a21af754bd47c7ad3cfe7cb1c4ed4c21
kelemul <kelemu3509@gmail.com>
2022-06-14 00:45:37 -0700

    done putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,27 @@
 /*
- * file:putchar
- * Author:kelemu
+ * File: 0-holberton.c
+ * Auth:Kelemu L.
+ *
  */
 
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Prints "_putchar".
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf( "_Putchar");
-	printf("\n");
-	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
