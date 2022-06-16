8b9b94a2a39c889eb65873dcb32deb03a18299da
kelemul <kelemu3509@gmail.com>
2022-06-15 22:26:22 -0700

    print numbers done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+/*
+ * File: 101-print_iber.c
+ * Auth: kelemu leykun
+ */
+
+#include "main.h"
+
+/**
+ * print_iber - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
+}
