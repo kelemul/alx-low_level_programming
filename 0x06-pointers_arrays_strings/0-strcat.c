93f62495d54706427293068568f46d8582133b4f
kelemul <kelemu3509@gmail.com>
2022-06-16 01:07:11 -0700

    all

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 /*
  * File: 0-strcat.c
+ * Auth: Kelemu L.
  */
 
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * strcat - Concatenates two stringss
+ * @dest: main string.
+ * @src: The other string
  *
  * Return: A pointer to the destination string @dest.
  */
