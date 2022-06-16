28890551ccc2404015ec18c8b0676ecbc39880ac
kelemul <kelemu3509@gmail.com>
2022-06-16 01:10:44 -0700

    done

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,23 @@
-/*
- * File: 101-print_iber.c
- * Auth: kelemu leykun
- */
+#include <stdio.h>
 
-#include "main.h"
-
-/**
- * print_iber - Prints an integer.
- * @n: The integer to be printed.
- */
-void print_number(int n)
+int main(void)
 {
-	unsigned int i = n;
+  int n;
+  int a[5];
+  int *p;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		i = -i;
-	}
-
-	if ((i / 10) > 0)
-		print_number(i / 10);
-
-	_putchar((i % 10) + '0');
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
