b3a13e685b1aa7fe877658f26883a2bc3a7d20e8
kelemul <kelemu3509@gmail.com>
2022-06-20 06:44:43 -0700

    first push on the new proj

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-memset.c
+ * Auth: KELEMU l.
+ */
+
+#include "main.h"
+
+/**
+ * memset - Fill the memory with pointer
+ * @s: A pointer.
+ * @c: The character to be filled.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: @s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
