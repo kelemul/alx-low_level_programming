b995559c4b8664f8310b1e380fd1f75607d2154c
kelemul <kelemu3509@gmail.com>
2022-06-20 07:24:09 -0700

    string position found

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,17 +1,16 @@
 /*
  * File: 4-strpbrk.c
- * Auth: Brennan D Baraban
+ * Auth: kelemu l
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
+ * _strpbrk - Searches for a strings.
  * @s: The string to be searched.
  * @accept: The set of bytes to be searched for.
  *
- * Return: If a set is matched - a pointer to the matched byte.
- *         If no set is matched - NULL.
+ * Return: A pointer a string otherwise - NULL.
  */
 char *_strpbrk(char *s, char *accept)
 {
