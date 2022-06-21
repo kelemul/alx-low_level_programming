d5f51fe196ebe2747d21740230709355d1666b56
kelemul <kelemu3509@gmail.com>
2022-06-21 01:46:57 -0700

    done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,20 @@
 /*
- * File: 4-strpbrk.c
- * Auth: kelemu L.
+ * File: 3-strspn.c
+ * Auth: Kelemu L.
  */
 
 #include "main.h"
 
 /**
- * _strpbrk - Searches a string
+ * _strspn - Gets substring lengh.
  * @s: The string to be searched.
- * @accept: The string to searched
+ * @accept: The sub string.
  *
- * Return: A string atherwise - NULL.
+ * Return: The length of the substring.
  */
-char *_strpbrk(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int bytes = 0;
 	int index;
 
 	while (*s)
@@ -21,11 +22,17 @@ char *_strpbrk(char *s, char *accept)
 		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index])
-				return (s);
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
 		}
 
 		s++;
 	}
 
-	return ('\0');
+	return (bytes);
 }
