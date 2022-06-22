e11b69c5078fbafcc8fe71fdc47dc86743f875e3
kelemul <kelemu3509@gmail.com>
2022-06-22 08:20:59 -0700

    square root done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int find_min(int num, int mid);
+int _sqrt_recursion(int n);
+
+int find_min(int num, int mid)
+{
+	if ((mid * mid) == num)
+		return (mid);
+
+	if (mid == num / 2)
+		return (-1);
+
+	return (find_min(num, mid + 1));
+}
+
+/**
+ * _sqrt_recursion - calculates the square mid of a number.
+ * @n: The number to be square mided.
+ *
+ * Return: The square mid of @n
+ */
+int _sqrt_recursion(int n)
+{
+	int mid = 0;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (find_min(n, mid));
+}
