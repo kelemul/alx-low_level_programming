cf6dffc96bed496d972cc24b228cc81a6c4eb71b
kelemul <kelemu3509@gmail.com>
2022-06-28 06:45:18 -0700

    memory allocation done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * craete_array - generate an array of characters
+ * @s: The s of array
+ * @c:  asingle char
+ *
+ * Return: NULL or the array
+ */
+char *create_array(unsigned int s, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (s == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * s);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < s; i++)
+		array[i] = c;
+
+	return (array);
+}
