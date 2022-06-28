1702706f1240d2473748ee9d9e779e8c9de01865
kelemul <kelemu3509@gmail.com>
2022-06-28 07:02:46 -0700

    concat done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str-concat - merges two strings
+ * @str1: main string
+ * @str2: the string to be merged
+ *
+ * Return: NULL or the two string
+ */
+char *str_concat(char *str1, char *str2)
+{
+	char *merged;
+	int i, index = 0, s = 0;
+
+	if (str1 == NULL)
+		str1 = "";
+
+	if (str2 == NULL)
+		str2 = "";
+
+	for (i = 0; str1[i] || str2[i]; i++)
+		s++;
+
+	merged = malloc(sizeof(char) * s);
+
+	if (merged == NULL)
+		return (NULL);
+
+	for (i = 0; str1[i]; i++)
+		merged[index++] = str1[i];
+
+	for (i = 0; str2[i]; i++)
+		merged[index++] = str2[i];
+
+	return (merged);
+}
