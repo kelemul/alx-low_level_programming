5ba8bda71524b4aba5c91ccdb07bc0e77900743e
kelemul <kelemu3509@gmail.com>
2022-07-05 01:44:50 -0700

    struct 1 done by me

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+/**
+ * struct dog - dog type
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - type of dog struct
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
