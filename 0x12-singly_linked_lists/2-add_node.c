720822abb7afda727109857f3033b57fe9a9988b
kelemul <kelemu3509@gmail.com>
2022-07-14 06:13:38 -0700

    adding new node done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - Add a new node at the beg.
+ * @head: first node pointer.
+ * @str: the value of the new node.
+ *
+ * Return: NULL or new element.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
