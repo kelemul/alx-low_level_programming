2967779eb7708de9cc9def4f2c0ab7a6d71e965a
kelemul <kelemu3509@gmail.com>
2022-07-21 05:09:37 -0700

    done binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - a function that converts binary to int
+ * @b: the pointer to a string
+ *
+ *Return: NULL or 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num, i;
+
+	if (!b || !*b)
+		return (0);
+
+	num = i = 0;
+	while (b[i])
+	{
+		if (b[i] > 49)
+			return (0);
+		else if (b[i] == 49)
+		{
+			num <<= 1;
+			num += 1;
+		}
+		else
+			num <<= 1;
+		i++;
+	}
+	return (num);
+}
